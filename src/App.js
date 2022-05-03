ae9a4bafe443a2b9ab196a70ffb1024ba0bf940e
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-05-03 10:46:23 +0200

    button css adjusted with size and click functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-
-
-import "./App.css";
 import Beer from "./components/Beer/Beer";
 import BeerContainer from "./components/BeerContainer/BeerContainer";
 
 function App() {
   return (
-
     <div className="App">
       <BeerContainer className="beer">
         <Beer
           favourite={true}
           userBeer={true}
-          name={"cerveza top"}
-          tagline={"esta cerve es cojonudisima"}
+          name={"Cerveza to"}
+          tagline={
+            "esta cerve es cojonudisima, pero de verdad que esta rica, buena como ninguna otra..."
+          }
         />
       </BeerContainer>
-
     </div>
   );
 }
