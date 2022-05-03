1a49b3195e431f3af9b3faab32ed1fbec1ce9430
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-05-03 17:55:59 +0200

    Replicated remote app.js to resolve conflict error import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import beer from "./BeerData";
+import Beer from "./components/Beer/Beer";
+import BeerContainer from "./components/BeerContainer/BeerContainer";
 import BeerDetails from "./components/BeerDetails/BeerDetails";
-import Beers from "./components/Beers/Beers";
+import BeerDetailsContainer from "./components/BeerDetailsContainer/BeerDetailsContainer";
+import Header from "./components/Header/Header";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
     <div className="App">
-      <Beers />
-      <BeerDetails beer={beer} />
+      <Header pageTitle={"Beer Directory"} />
+      <Search />
+      <BeerContainer className="beer">
+        <Beer
+          favourite={true}
+          userBeer={true}
+          name={"Cerveza to"}
+          tagline={
+            "esta cerve es cojonudisima, pero de verdad que esta rica, buena como ninguna otra..."
+          }
+        />
+      </BeerContainer>
+      <BeerDetailsContainer>
+        <BeerDetails beer={beer} />
+      </BeerDetailsContainer>
     </div>
   );
 }
