c8c18280edaa1a8a4b2e68413a050dc1c7bb1e20
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-05-03 19:32:01 +0200

    fixing test of Beers component

--- src/components/Beers/Beers.test.js
+++ src/components/Beers/Beers.test.js
@@ -2,12 +2,12 @@ import { render, screen } from "@testing-library/react";
 import Beers from "./Beers";
 
 describe("Given the Beers component", () => {
-  describe("When it receives 1 beer", () => {
-    test("Then it should render 1 beer card", () => {
-      const expectedNumber = 1;
+  describe("When it receives 4 beer", () => {
+    test("Then it should render 4 beer card", () => {
+      const expectedNumber = 4;
 
       render(<Beers />);
-      const beerCardsNumber = screen.getAllByRole("list");
+      const beerCardsNumber = screen.getAllByRole("listitem");
 
       expect(beerCardsNumber.length).toBe(expectedNumber);
     });
