306223153555468ba5bb3747573db4c6fabea67d
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-05-03 16:49:50 +0200

    Added styled component to search

---
+++ src/components/SearchInput/SearchInput.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const SearchInput = styled.div`
+  padding: 30px 5px 20px 5px;
+  width: 85%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+
+  input {
+    width: 90%;
+    height: 25px;
+    border-radius: 5px;
+    text-indent: 10px;
+  }
+`;
+export default SearchInput;
