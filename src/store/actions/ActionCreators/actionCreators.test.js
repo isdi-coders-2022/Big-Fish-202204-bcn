9ad298c142233bc474dd861c9684c4f7c87b79ca
MariaDelMar23 <maria.cegarra2303@gmail.com>
2022-05-04 10:28:02 +0200

    Tested toggleFavouriteActionCreator and toggleFavouriteActionType

---
+++ src/store/actions/ActionCreators/actionCreators.test.js
@@ -0,0 +1,15 @@
+import { toggleFavouriteActionType } from "../actionTypes";
+import { toggleFavouriteActionCreator } from "./actionCreator";
+
+describe("Given the toggleFavouriteActionCreator function", () => {
+  describe("When it receives a 7", () => {
+    test("Then it should return an action object with properties type: 'toggle-favourite', id:7", () => {
+      const id = 7;
+      const expectedAction = { type: toggleFavouriteActionType, id: id };
+
+      const action = toggleFavouriteActionCreator(7);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
