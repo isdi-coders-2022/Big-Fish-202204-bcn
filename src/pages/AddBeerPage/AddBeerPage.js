f3220597fc9c74e6646e742eafde0991a0e9bee0
MariaDelMar23 <maria.cegarra2303@gmail.com>
2022-05-04 14:06:32 +0200

    Created pages (MyBeers, EditBeers, BeerList, BeerDetails, AddBeer)

---
+++ src/pages/AddBeerPage/AddBeerPage.js
@@ -0,0 +1,11 @@
+import Header from "../../components/Header/Header";
+
+const AddBeerPage = () => {
+  return (
+    <div className="AddBeerPage">
+      <Header pageTitle={"Add New Beer"} />
+    </div>
+  );
+};
+
+export default AddBeerPage;
---
+++ src/pages/EditBeerPage/EditBeerPage.js
@@ -0,0 +1,11 @@
+import Header from "../../components/Header/Header";
+
+const EditBeerPage = () => {
+  return (
+    <div className="EditBeerPage">
+      <Header pageTitle={"Edit Beer"} />
+    </div>
+  );
+};
+
+export default EditBeerPage;
