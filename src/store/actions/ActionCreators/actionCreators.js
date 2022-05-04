da304ce1dd997e8285f93a1a0993d8ee7895f3f0
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-05-04 10:45:50 +0200

    Added delete actiontype and action creator

--- src/store/actions/ActionCreators/actionCreators.js
+++ src/store/actions/ActionCreators/actionCreators.js
@@ -1,6 +1,12 @@
 import { toggleFavouriteActionType } from "../actionTypes";
+import { deleteBeerActionType } from "../actionTypes";
 
 export const toggleFavouriteActionCreator = (id) => ({
   type: toggleFavouriteActionType,
+  id,
+});
+
+export const deleteBeerActionCreator = (id) => ({
+  type: deleteBeerActionType,
   id,
 });
