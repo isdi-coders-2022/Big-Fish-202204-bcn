69ceb1bfc26c562085d8c8358b2422b19bdd85f2
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-05-05 10:05:37 +0200

    resaved files with renamed folder

--- src/components/HeaderContainer/HeaderContainer.js
+++ src/components/HeaderContainer/HeaderContainer.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { appColor } from "../../mixings/elementsColours";
+import { appColor } from "../../colours/elementsColours";
 const headerBckgroundColor = appColor;
 
 const HeaderContainer = styled.header`
