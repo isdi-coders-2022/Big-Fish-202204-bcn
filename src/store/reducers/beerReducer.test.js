6d24f87df504f69aad1441ddb3c0b3eca71e8b27
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-08 20:02:21 +0200

    Tested loadLocalBeersAction from reducer

--- src/store/reducers/beerReducer.test.js
+++ src/store/reducers/beerReducer.test.js
@@ -4,6 +4,7 @@ import {
   toggleFavouriteActionCreator,
   editBeerActionCreator,
   loadBeersActionCreator,
+  loadLocalBeersActionCreator,
 } from "../actions/ActionCreators/actionCreators";
 import beerReducer from "./beerReducer";
 
@@ -108,6 +109,26 @@ describe("Given the beerReducer component", () => {
       ];
 
       const action = loadBeersActionCreator(beersGrup);
+      const resultGrup = beerReducer(beersGrup, action);
+
+      expect(resultGrup).toEqual(expectedBeersGrup);
+    });
+  });
+
+  describe("When it receives a grup of beers and the order to load from LocalApi", () => {
+    test("Then it should return a same grup of beers with the news propieties userBeer,favourite,rating", () => {
+      const beersGrup = [
+        { id: 0, text: "UwU", userBeer: false, favourite: false, rating: 0 },
+        { id: 1, text: "UwU", userBeer: false, favourite: false, rating: 0 },
+        { id: 2, text: "UwU", userBeer: false, favourite: false, rating: 0 },
+      ];
+      const expectedBeersGrup = [
+        { id: 0, text: "UwU", userBeer: false, favourite: false, rating: 0 },
+        { id: 1, text: "UwU", userBeer: false, favourite: false, rating: 0 },
+        { id: 2, text: "UwU", userBeer: false, favourite: false, rating: 0 },
+      ];
+
+      const action = loadLocalBeersActionCreator(beersGrup);
       const resultGrup = beerReducer(beersGrup, action);
 
       expect(resultGrup).toEqual(expectedBeersGrup);
