5bf44314c87ca4e0c367550ebc18c6c1bf3e4cf7
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-08 19:15:13 +0200

    Tested addBeer and DeleteBeer from useApiMyBeers hook

--- src/hooks/useApiMyBeers.test.js
+++ src/hooks/useApiMyBeers.test.js
@@ -1,7 +1,8 @@
-import { renderHook, waitFor } from "@testing-library/react";
+import { render, renderHook, waitFor } from "@testing-library/react";
 import BeerProvider from "../store/context/BeerProvider";
 import useApiMyBeers from "./useApiMyBeers";
 import beers from "../data/beers";
+import beer from "../data/beer";
 
 describe("Given the useApiMyBeers hook", () => {
   describe("When the getMyBeers function is called", () => {
@@ -13,6 +14,32 @@ describe("Given the useApiMyBeers hook", () => {
       const resultBeers = await waitFor(() => result.current.getLocalBeers());
 
       expect(resultBeers).toEqual(expectedBeers);
+    });
+  });
+
+  describe("When the addBeer function is called", () => {
+    test("Then it should return an object to add", async () => {
+      const expectedBeer = { ...beer };
+      const wrapper = ({ children }) => <BeerProvider>{children}</BeerProvider>;
+
+      const { result } = renderHook(() => useApiMyBeers(), { wrapper });
+      const resultBeer = await waitFor(() => result.current.addBeer(beer));
+
+      expect(resultBeer).toEqual(expectedBeer);
+    });
+  });
+
+  describe("When the deleteBeer function is called with the id to delete", () => {
+    test("Then it should return an id", async () => {
+      const expectedBeerId = beer.id;
+      const wrapper = ({ children }) => <BeerProvider>{children}</BeerProvider>;
+
+      const { result } = renderHook(() => useApiMyBeers(), { wrapper });
+      const resultBeerId = await waitFor(() =>
+        result.current.deleteBeer(beer.id)
+      );
+
+      expect(resultBeerId).toBe(expectedBeerId);
     });
   });
 });
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,15 +1,23 @@
 import { rest } from "msw";
 import beers from "../data/beers";
-import { endpoint } from "../paths/endpoint";
-const urlApi = process.env.REACT_APP_API_URL;
-const urlApiLocal = process.env.REACT_APP_API_LOCAL_URL;
+import beer from "../data/beer";
 
 export const handlers = [
   rest.get(
     "https://api.punkapi.com/v2/beers?page=1&per_page=12",
     (_req, res, ctx) => res(ctx.status(200), ctx.json([...beers]))
   ),
+
   rest.get(`https://mybeers-api.onrender.com/myBeers`, (_req, res, ctx) =>
     res(ctx.status(200), ctx.json([...beers]))
+  ),
+
+  rest.post(`https://mybeers-api.onrender.com/myBeers`, (_req, res, ctx) =>
+    res(ctx.status(200), ctx.json({ ...beer }))
+  ),
+
+  rest.delete(
+    `https://mybeers-api.onrender.com/myBeers/${beer.id}`,
+    (_req, res, ctx) => res(ctx.status(200), ctx.json(beer.id))
   ),
 ];
